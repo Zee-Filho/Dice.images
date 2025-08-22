6a46618d0912aab1fb6ae94ccfe67e76358b94dd
Zee_Filho <zeefilhopt@gmail.com>
2025-08-22 12:05:16 +0100

    Simplify the boilerplate app

---
+++ my-app/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="" alt="logo" />
+        <p>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
